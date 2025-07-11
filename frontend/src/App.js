6fda2b0024ec96521cbeeb603ec60a9c08e0672d
Satyam Saini <stym.6996@gmail.com>
2025-07-11 23:46:27 +0530

    Removed not needed pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
 import ProfessionalJourney from './pages/ProfessionalJourney';
 import Contact from './pages/Contact';
 import Navbar from "./components/Navbar";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
@@ -14,9 +14,9 @@ function App() {
         <main className="container mx-auto px-4 py-8 fade-in">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
             <Route path="/experience" element={<ProfessionalJourney />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/projects" element={<Projects />} />
           </Routes>
         </main>
       </div>
